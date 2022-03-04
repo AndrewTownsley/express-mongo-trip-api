838482a12e9f93b43856cf6c47a955daa9821bb8
Andrew TOwnsley <atownsley86@gmail.com>
2022-03-04 16:20:39 -0700

    Beginning to add Mongodb connection

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import { MongoClient } from "mongodb";
 import 'dotenv/config';
 import tripRoutes from './routes/trips.js'
 
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+// - Make the trip expense tracker into a fullstack app.
+// 		- Make it like a yelp or tripadvisor type site.  Message board for travel.
+// 		- Add photo or video upload capability
+//      - geolocation capability
+// 	- Try to figure out as much as you can by yourself
+////////////////////////////////////////////////////////////
 const app = express();
 
 app.use(express());
